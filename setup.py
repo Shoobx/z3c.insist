8ab615f032072858ab20cb1656f67f60b680d5ca
Adam Groszer <agroszer@gmail.com>
2015-10-30 12:12:33 +0100

    fix version,  PEP-386.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_file(filename):
 
 setup(
     name="z3c.insist",
-    version='0.1.0dev9',
+    version='0.1.0.dev9',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Persistence to ini files",
