3c8f5f0f2be42f7c7ed327f757560032ba0f4ee1
Stephan Richter <stephan.richter@gmail.com>
2017-05-25 06:55:57 -0400

    Back to development: 1.1.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read_file(filename):
 
 setup(
     name="z3c.insist",
-    version='1.1.3',
+    version='1.1.4.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Persistence to ini Files",
