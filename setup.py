c8d48aa3eae9badaa92359d6ff5cfed60958b38c
Justas Sadzevicius <jsadzevicius@shoobx.com>
2020-12-15 11:12:24 +0200

    Python 3.5 reached end-of-life on Sept. 5, 2020

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
