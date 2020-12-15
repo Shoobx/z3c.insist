00ef5d2ff57797db319fc277baba5bcaa8f6d890
Justas Sadzevicius <jsadzevicius@shoobx.com>
2020-12-15 11:08:21 +0200

    Add Python 3.8 support

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Natural Language :: English',
